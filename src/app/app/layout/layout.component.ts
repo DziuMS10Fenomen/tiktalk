a4964551d9e5fd2ac4358794cb297d11a6964a0c
Andrii Dziura <82643719+andrii-dziu@users.noreply.github.com>
2025-01-24 16:57:13 +0100

    fix: code

--- src/app/app/layout/layout.component.ts
+++ src/app/app/layout/layout.component.ts
@@ -2,25 +2,19 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileService } from '../../data/services/profile.service';
 
-
 @Component({
   selector: 'app-layout',
   imports: [RouterOutlet],
-  providers:[ProfileService],
+  standalone: true, 
+  providers: [ProfileService],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
+  profileService = inject(ProfileService);
 
-  
-   profileService= inject(ProfileService)
-
-   ngOnInit(){
-     this.profileService.getMe().subscribe({
-
-     })
-
-    
+  ngOnInit() {
+    this.profileService.getMe().subscribe( data =>{
+    });
   }
 }
-
